872ac90817c61691a0cfdeda3c50210172e4eccc
analourdescr <ana_lourdes98@hotmail.com>
2022-09-15 07:13:24 -0700

    Initial Commit

---
+++ navegacion/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  navegacion
+//
+//  Created by Alumno on 9/15/22.
+//  Copyright © 2022 Alumno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
